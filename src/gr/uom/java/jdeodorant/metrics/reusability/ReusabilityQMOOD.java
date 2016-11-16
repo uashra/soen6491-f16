0ada3e0bf2a056fdfbbc2ee301dc93931c52b299
Valeriu Caramanuta <kavaleriu@gmail.com>
2016-11-15 22:58:57 -0500

    calculate cohesion among methods

--- src/gr/uom/java/jdeodorant/metrics/reusability/ReusabilityQMOOD.java
+++ src/gr/uom/java/jdeodorant/metrics/reusability/ReusabilityQMOOD.java
@@ -1,6 +1,7 @@
 package gr.uom.java.jdeodorant.metrics.reusability;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -8,6 +9,7 @@ import java.util.Set;
 import gr.uom.java.ast.MethodObject;
 import gr.uom.java.ast.Access;
 import gr.uom.java.ast.ClassObject;
+import gr.uom.java.ast.FieldInstructionObject;
 import gr.uom.java.ast.SystemObject;
 
 // Implementation for QMOOD reusability metric 
@@ -21,13 +23,25 @@ public class ReusabilityQMOOD {
 	}
 
 	private static double calculateDesignSize(SystemObject system) {
-		
 		return system.getClassObjects().size();
 	}
 
 	private static double calculateCohesion(SystemObject system) {
-		// TODO Auto-generated method stub
-		return 0;
+		Map<String, Double> cohesionMap = new HashMap<String, Double>();
+		Set<ClassObject> classes = system.getClassObjects();
+		
+		for (ClassObject classObject : classes) {
+			double classCohesion = calculateCohesion(classObject);
+			cohesionMap.put(classObject.getName(), classCohesion);
+
+		}
+		
+		double sumCohesion = 0.0;
+		for(String key : cohesionMap.keySet()) {
+			sumCohesion += cohesionMap.get(key);
+        	System.out.println( key + "  " +  cohesionMap.get(key));
+        }
+		return sumCohesion/classes.size();
 	}
 
 	private static double calculateMessaging(SystemObject system) {
@@ -64,6 +78,33 @@ public class ReusabilityQMOOD {
 
 	private static double calculateCoupling(SystemObject system) {
 		return 0.0;
+	}
+	
+	private static double calculateCohesion(ClassObject classObject){
+		List<MethodObject> methods = classObject.getMethodList();
+		Set<String> allParameters = new HashSet<String>();
+		
+		for (int i = 0; i < methods.size() - 1; i++) {
+			List<FieldInstructionObject> methodParameters = methods.get(i).getFieldInstructions();
+			for (FieldInstructionObject param : methodParameters)
+				allParameters.add(param.getType().toString());
+			
+		}
+		
+		if (allParameters.size() == 0)
+			return 0;
+		
+		double sumIntersection = 0.0;
+		
+		for (int i = 0; i < methods.size() - 1; i++) {
+			List<FieldInstructionObject> methodParameters = methods.get(i).getFieldInstructions();
+			for (FieldInstructionObject param : methodParameters)
+				if (allParameters.contains(param.getType().toString()))
+						sumIntersection++;
+			
+		}		
+		
+		return sumIntersection/(methods.size() + allParameters.size());
 	}
 
 }
